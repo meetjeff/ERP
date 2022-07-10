729ec4e65293b9d74533c64257d7aad24eb9ad8e
adf159776 <106952571+adf159776@users.noreply.github.com>
2022-07-10 15:07:29 +0800

    Create sta.py

---
+++ sta.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+def success(data=None):
+    if data is None:
+        return {'message': 'success'}, 200
+
+    return {
+        'message': 'success',
+        'data': data,
+        'datatime': datetime.utcnow().isoformat()
+    }, 200
+
+def failure(message):
+    return {"message": "failure,"+message}, 500
