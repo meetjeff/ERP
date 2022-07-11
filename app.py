613923f9e450a76552dfe02f2a48a437012ac65c
adf159776 <106952571+adf159776@users.noreply.github.com>
2022-07-11 11:37:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from flask_apispec.extension import FlaskApiSpec
-from resource import Punch,Count,Curriculum
+from resource import Punch,Count,Curriculum,Leave,Course
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,15 +14,12 @@ app.config.update({
         plugins=[MarshmallowPlugin()],
         openapi_version='2.0.0'
     ),
-    'APISPEC_SWAGGER_URL': '/api_doc',  # URI to access API Doc JSON
-    'APISPEC_SWAGGER_UI_URL': '/', # URI to access UI of API Doc
-    'JSON_AS_ASCII': False
+    'APISPEC_SWAGGER_URL': '/api_doc',
+    'APISPEC_SWAGGER_UI_URL': '/',
+    'JSON_AS_ASCII': False,
+    'MAX_CONTENT_LENGTH': 1024 * 1024
 })
 docs = FlaskApiSpec(app)
-
-@app.route('/upload')
-def upload():
-    return render_template('upload.html')
 
 api.add_resource(Punch,'/punch')
 docs.register(Punch)
@@ -30,7 +27,10 @@ api.add_resource(Count,'/count')
 docs.register(Count)
 api.add_resource(Curriculum,'/curriculum')
 docs.register(Curriculum)
-
+api.add_resource(Leave,'/Leave')
+docs.register(Leave)
+api.add_resource(Course,'/course')
+docs.register(Course)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
