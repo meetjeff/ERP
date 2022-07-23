ed2a437bdafcd5762103e8c7f000f4fb0e1a5e04
adf159776 <106952571+adf159776@users.noreply.github.com>
2022-07-23 15:05:49 +0800

    Create article.py

---
+++ article/article.py
@@ -0,0 +1,84 @@
+from bs4 import BeautifulSoup as bs
+import requests
+from lxml import html
+from googlesearch import search
+import pymysql
+import pandas as pd
+import sys
+import logging
+import os
+import re
+
+logging.basicConfig(filename = "article.log",
+                    level = logging.DEBUG,
+                    format = "%(asctime)s %(message)s",
+                    datefmt = "%d/%m/%Y %I:%M:%S %p")
+
+savepath = "~/article.log"
+if not os.path.exists(savepath):
+    logging.warning("Warning! The savepath provided doesn\'t exist!Saving at current directory")
+    savepath = os.getcwd() 
+    logging.info("savepath reset at %s",str(savepath))
+else:
+    logging.info("Savepath provided is correct,saving at %s",str(savepath))
+
+group=sys.argv[1]
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
+db = pymysql.connect(host = 'ec2-34-208-156-155.us-west-2.compute.amazonaws.com', port = 3306, user = 'erp', passwd = 'erp')
+cursor = db.cursor()
+sql = f"SELECT DISTINCT course FROM curriculum.{group} WHERE course not REGEXP'專題|輔導|產品|企業|研討會|典禮';" #篩掉部分關鍵字
+cursor.execute(sql)
+data = cursor.fetchall()
+df = pd.DataFrame(list(data))
+list_of_curriculum = df[0].tolist()
+
+for cur in list_of_curriculum:
+    query = cur+" 教學" #用課程名稱+教學避免查出官方網站或官方文件
+    try:
+        for i in search(query, stop = 3, pause = 1.0):  #query表示關鍵字，stop表示查詢筆數，pause表示查詢停留時間
+            if 'youtube' in i:  #跳過三個常常出現的不相干網站
+                pass
+            elif 'udemy' in i:
+                pass
+            elif 'accupass' in i:
+                pass
+            else:
+                try:
+                    res1 = requests.get(i, headers = headers) 
+                    res1.encoding = res1.apparent_encoding #通過res1.apparent_encoding屬性指定編碼
+                    html = res1.text
+                    soup = bs(html, 'lxml')
+                    if 'ithelp' in i:  #因為it邦幫忙的標題格式長得特別不一樣，又常常是搜尋結果，固有另外拉出來做處理
+                        search_t = soup.find('h2')
+                        search_title = search_t.text
+                    else:    
+                        search_t = soup.find('title')  #一般文章的標題格式
+                        search_title = search_t.text
+                    try:  
+                        arc1 = f"INSERT INTO curriculum.resource (groups, course, url, content, title) VALUES ('{group}', '{cur}', '{i}', 'article', '{search_title}')"
+                        cursor.execute(arc1)
+                        db.commit()
+                        
+                    except Exception as e:
+                        logging.info(e)
+                        
+                except:
+                    search_title = "參考資料"  #還有一些標題格式比較不一樣的，就沒有特別處理，標題就直接叫”參考資料“
+                    try:
+                        arc2 = f"INSERT INTO curriculum.resource (groups, course, url, content, title) VALUES ('{group}', '{cur}', '{i}', 'article', '{search_title}')"
+                        cursor.execute(arc2)
+                        db.commit()
+                        
+                    except Exception as e:
+                        logging.info(e)
+                       
+    except Exception as e:
+        logging.info(e)
+
+    
+fin = f"INSERT INTO curriculum.crawlerstatus (groups,articles) VALUES('{group}','finished') ON DUPLICATE KEY UPDATE videos=videos,articles='finished';"
+cursor.execute(fin)
+db.commit()
+cursor.close()   
+db.close()
+logging.info("%s article finished",str(group))
