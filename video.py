46a3d7fd6cc2f16aa0058b6f4556c3bf2180a491
adf159776 <106952571+adf159776@users.noreply.github.com>
2022-07-23 15:18:03 +0800

    Update video.py

--- video.py
+++ video.py
@@ -8,6 +8,19 @@ import logging
 import os
 import re
 
+logging.basicConfig(filename = "video.log",
+                    level = logging.DEBUG,
+                    format = "%(asctime)s %(message)s",
+                    datefmt = "%d/%m/%Y %I:%M:%S %p")
+
+savepath = "~/video.log"
+if not os.path.exists(savepath):
+    logging.warning("Warning! The savepath provided doesn\'t exist!Saving at current directory")
+    savepath = os.getcwd() 
+    logging.info("savepath reset at %s",str(savepath))
+else:
+    logging.info("Savepath provided is correct,saving at %s",str(savepath))
+
 group=sys.argv[1]
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
 db1 = pymysql.connect(host = 'ec2-34-208-156-155.us-west-2.compute.amazonaws.com', port = 3306, user = 'erp', passwd = 'erp')
@@ -21,28 +34,25 @@ list_of_curriculum1 = df1[0].tolist()
 for cur1 in list_of_curriculum1:                    
     queryv = cur1 + "youtube"
     try:
-        for i2 in search(queryv, stop = 1, pause = 1.0):
-            print(i2)
+        for i2 in search(queryv, stop = 3, pause = 1.0):
             if 'https://www.youtube.com/watch?v=' in i2:  
                 try:
                     res2 = requests.get(i2, headers = headers)
                     res2.encoding = res2.apparent_encoding #通過res.apparent_encoding屬性指定編碼
                     url = re.sub("watch\?v=","embed/", i2)
                     yt = f"INSERT INTO curriculum.resource (groups, course, url, content, title) VALUES ('{group}', '{cur1}', '{url}', 'video', '{cur1}')"
-                    # lock.acquire()
                     cursor1.execute(yt)
                     db1.commit()
-                    # lock.release()
+                   
                 except Exception as e:
-                    # logging.info(e) 
-                    print(e)
+                    logging.info(e) 
+                    
     except Exception as e:
-        print(e)
+        logging.info(e)
 
-fin = f"INSERT INTO curriculum.crawlerstatus (groups, status) VALUES('{group}', 'finished') ON DUPLICATE KEY UPDATE status='finished';"
+fin = f"INSERT INTO curriculum.crawlerstatus (groups,videos) VALUES('{group}','finished') ON DUPLICATE KEY UPDATE videos='finished',articles=articles;"
 cursor1.execute(fin)
 db1.commit()
 cursor1.close()   
 db1.close()
-# logging.info("%s finished",str(group))
-print("ytfinish")
+logging.info("%s video finished",str(group))
