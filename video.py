6e893d2c8ba4eb3e72166b5a039b9498cac60ede
Jeff Hsu <106952571+adf159776@users.noreply.github.com>
2022-08-07 06:16:20 +0800

    Update video.py

--- video.py
+++ video.py
@@ -1,5 +1,3 @@
-import requests
-from lxml import html
 from googlesearch import search
 import pymysql
 import pandas as pd
@@ -26,7 +24,6 @@ else:
     logging.info("Savepath provided is correct,saving at %s",str(savepath))
 
 group=sys.argv[1]
-headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
 db1 = pymysql.connect(host = os.getenv("dbip"), port = int(os.getenv("dbport")), user = os.getenv("dbuser"), passwd = os.getenv("dbpassword"))
 cursor1 = db1.cursor()
 sql1 = f"SELECT DISTINCT course FROM curriculum.{group} WHERE course not REGEXP'專題|輔導|產品|企業|研討會|典禮|結訓';" #篩掉部分關鍵字
@@ -41,13 +38,12 @@ for cur1 in list_of_curriculum1:
         for i2 in search(queryv, stop = int(os.getenv("search")), pause = 1.0):
             if 'https://www.youtube.com/watch?v=' in i2:  
                 try:
-                    res2 = requests.get(i2, headers = headers)
-                    res2.encoding = res2.apparent_encoding #通過res.apparent_encoding屬性指定編碼
-                    time.sleep(random.uniform(1, 2))
-                    url = re.sub("watch\?v=","embed/", i2)
+                    watch = re.sub("watch\?v=","embed/", i2)
+                    url = re.sub("&.*","", watch)
                     yt = f"INSERT INTO curriculum.resource (groups, course, url, content, title) VALUES ('{group}', '{cur1}', '{url}', 'video', '{cur1}')"
                     cursor1.execute(yt)
                     db1.commit()
+                    time.sleep(random.uniform(1, 2))
                    
                 except Exception as e:
                     logging.info(e) 
