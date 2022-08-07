76be239015b8c3dd7878c0eca0f23c01f44456ae
Jeff Hsu <106952571+adf159776@users.noreply.github.com>
2022-08-08 01:33:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 bind = "0.0.0.0:8080"
-workers = 3
+#workers = 3
 reload = True
 worker_class = "gevent"
 backlog = 2048
-accesslog = "/home/ec2-user/punch.log"
+accesslog = "/home/punch/punch.log"
 loglevel = "debug"
+access_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
